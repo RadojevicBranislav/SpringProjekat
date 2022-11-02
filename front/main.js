5f1fc175cef6bfa475c4a0e0683e523317032de1
Branislav Radojevic <branislavradojevicit4@gmail.com>
2022-11-02 21:49:33 +0100

    Dodat frontend, nije testiran da li radi

---
+++ front/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    works();
+});
+
+
+
+function works(){
+
+    fetch('http://localhost:8080/artists')
+    .then((response) =>{ response.json();
+    })
+    .then((myJson) => console.log(json));
+
+}
+function generateCards()
+{
+
+    fetch('http://10.20.1.52:8080/artists')
+    .then((response) =>{ response.json();
+    })
+    .then((myJson) => {
+        var innerHtml = "";
+        var table = document.getElementById("cards");
+
+        $.each(myJson, function(index, artist){
+            
+            innerHtml+=`
+            <div class="card col col-sm-12 col-md-6 col-lg-4">
+            <div class="card-body">
+                <h5 class="card-title">${artist.id} type: ${artist.type}</h5>
+                <h5 class="card-title">${artist.fistName}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${artists.lastName}</h6>
+            </div>
+            </div>
+            `
+
+        })
+
+        table.innerHTML = innerHtml;
+    });
+
+}
